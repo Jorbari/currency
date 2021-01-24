a69aa2fea59da90ae4c44d2c5c5c00c61ed98f03
Jorbari <jorbarinaaton1@gmail.com>
2021-01-24 14:00:04 +0100

    add sort functionality

--- src/App.jsx
+++ src/App.jsx
@@ -108,6 +108,24 @@ class App extends React.Component {
 
   sort_list_apha_order = () => {
     
+    // const hold = this.state.currencies.sort((a, b) => (a.name.toLowerCase() > b.name.toLowerCase()) ? 1 : (a.name.toLowerCase() === b.name.toLowerCase()) ? ((a.code > b.code) ? 1 : -1) : -1 );
+    // console.table(hold)
+
+    const hold = this.state.currencies.sort(function(a, b) {
+      var nameA = a.name.toUpperCase(); // ignore upper and lowercase
+      var nameB = b.name.toUpperCase(); // ignore upper and lowercase
+      if (nameA < nameB) {
+        return -1;
+      }
+      if (nameA > nameB) {
+        return 1;
+      }
+    
+      // names must be equal
+      return 0;
+    });
+
+    console.table(hold)
 
 
   }
